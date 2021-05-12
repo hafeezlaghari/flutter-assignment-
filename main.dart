2f27dc1d0a80a5386c68cdc6f5e3d670b6b61f71
hafeezlaghari <84062100+hafeezlaghari@users.noreply.github.com>
2021-05-12 12:33:20 -0700

    initial commit

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Text('Hafeez Ul Rehman Laghari  Lagharihafeez064@gmail.com')
+      ),
+    );
+  }
+}
+
+
